782ad9ce1418aad41917e823a28ce976786b8a7a
Chris Johnson <johnsonc.88@gmail.com>
2016-07-16 12:45:07 -0700

    change index variable to i

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -2,16 +2,16 @@ puts "What secret would you like to keep?"
 input = gets.chomp
 
 def encode(secret)
-	index = 0
+	i = 0
 
 	# determine string length
 	length = secret.length
 	# iterate through the string
-	while index < length
+	while i < length
 		# change each letter to the next letter in the alphabet
-		secret[index] = secret[index].next
+		secret[i] = secret[i].next
 		# overwrite the old string with the new string
-		index += 1
+		i += 1
 	end
 
 	puts secret
@@ -27,16 +27,16 @@ encoded = encode(input)
 def decode(encoded_secret)
 	# make alphabet string
 	alphabet = "abcdefghijklmnopqrstuvwxyz"
-	index = 0
+	i = 0
 
-	while index < encoded_secret.length
+	while i < encoded_secret.length
 		# get next letter in input
-		letter = encoded_secret[index]
+		letter = encoded_secret[i]
 		# find it in the alphabet
 		new_letter = alphabet[alphabet.index(letter) - 1]
 		# change it to the preceeding letter
-		encoded_secret[index] = new_letter
-		index += 1
+		encoded_secret[i] = new_letter
+		i += 1
 	end
 
 	puts encoded_secret
