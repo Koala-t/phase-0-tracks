681726ad134673d8ed9938fb5824717dd6fc07af
Chris Johnson <johnsonc.88@gmail.com>
2016-07-17 15:31:22 -0700

    update with work from pairing

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,6 +1,3 @@
-puts "What secret would you like to keep?"
-input = gets.chomp
-
 def encode(secret)
 	i = 0
 
@@ -8,52 +5,64 @@ def encode(secret)
 	length = secret.length
 	# iterate through the string
 	while i < length
-		# change each letter to the next letter in the alphabet
-		secret[i] = secret[i].next
-		# overwrite the old string with the new string
+		# if the letter is "z"
+		if secret[i] == "z"
+			# change it to "a"
+			secret[i] = "a"
+		else  
+		# otherwise change the letter to the next letter in the alphabet
+			secret[i] = secret[i].next
+		end
 		i += 1
 	end
 
-	puts secret
+	#puts secret
 	return secret
 end
-
-encoded = encode(input)
-
-
-
 
 # declare method
 def decode(encoded_secret)
 	# make alphabet string
 	alphabet = "abcdefghijklmnopqrstuvwxyz"
-	i = 0
 
+	i = 0
 	while i < encoded_secret.length
 		# get next letter in input
 		letter = encoded_secret[i]
-		# find it in the alphabet
-		new_letter = alphabet[alphabet.index(letter) - 1]
-		# change it to the preceeding letter
-		encoded_secret[i] = new_letter
+		# if the letter is "a"
+		if letter == "a"
+			# change it to "z"
+			letter = "z"
+		else
+			# find the letter in the alphabet and change it to the preceeding letter
+			encoded_secret[i]= alphabet[alphabet.index(letter) - 1]
+		end
 		i += 1
 	end
 
-	puts encoded_secret
+	#puts encoded_secret
+	return encoded_secret
 # end method
 end
 
+#decode(encode("swordfish"))
 
-puts "would you like to decode the secret? (y/n)"
-response = gets.chomp
+#This works because the encode method evaluates 
+#to a string which the decode method can then evaluate.
 
-if response == "y"
-	decode(encoded)
+
+
+#ask the user if they would like to encode or decode a password
+puts "Would you like to encode or decode a password?"
+operation = gets.chomp
+
+#ask them for the password
+puts "What is the password?"
+password = gets.chomp
+#if they want to encode, run that program
+if operation == "encode"
+	puts encode(password)
+#if they want to decode, run that program
+elsif operation == "decode"
+	puts decode(password)
 end
-
-
-
-
-
-
-
