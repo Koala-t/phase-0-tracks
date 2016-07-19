75cff34092af73c9cbd780928708dba72458e139
Chris Johnson <johnsonc.88@gmail.com>
2016-07-19 13:54:02 -0700

    write pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,22 @@
+# Program to store client details
+
+# make a method to ask for and store a users...
+
+# ...name
+# ...age
+# ...favorite color
+# ...number of children
+	# convert answer to an integer
+# ...do they think lead poisioning is a myth
+	# convert answer to boolean
+
+# print the hash to the screen
+
+# ask the user if all the info is correct
+	# if no...
+		# ask which field is incorrect
+		# update it 
+	# if yes...
+		# move on
+
+# print the hash again
