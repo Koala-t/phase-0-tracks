2710e19cde3f63cf47877917de58639e65057365
Chris Johnson <johnsonc.88@gmail.com>
2016-07-19 14:04:16 -0700

    gather client info

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -2,14 +2,25 @@
 
 # make a method to ask for and store a users...
 def Client_info
-
+	info = {}
 	# ...name
+	puts "Please enter your name:"
+		info[:name] = gets.chomp
 	# ...age
+	puts "How old are you:"
+		# convert to an integer
+		info[:age] = gets.chomp.to_i
 	# ...favorite color
+	puts "What is your favorite color:"
+		info[:color] = gets.chomp
 	# ...number of children
+	puts "How many children do you have:"
 		# convert answer to an integer
+		info[:children] = gets.chomp.to_i
 	# ...do they think lead poisioning is a myth
+	puts "You don't buy that whole 'lead is bad for you' malarkey do you?(y/n)"
 		# convert answer to boolean
+		info[:lead] = gets.chomp
 
 	# print the hash to the screen
 
@@ -21,5 +32,7 @@ def Client_info
 			# move on
 
 	# print the hash again
-
+	puts info
 end 
+
+Client_info()
