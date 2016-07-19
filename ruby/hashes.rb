f07f39cec5095a8a9736a9864f10a3120baaefe5
Chris Johnson <johnsonc.88@gmail.com>
2016-07-19 13:56:46 -0700

    declare method and indent pseudocode

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,22 +1,25 @@
 # Program to store client details
 
 # make a method to ask for and store a users...
+def Client_info
 
-# ...name
-# ...age
-# ...favorite color
-# ...number of children
-	# convert answer to an integer
-# ...do they think lead poisioning is a myth
-	# convert answer to boolean
+	# ...name
+	# ...age
+	# ...favorite color
+	# ...number of children
+		# convert answer to an integer
+	# ...do they think lead poisioning is a myth
+		# convert answer to boolean
 
-# print the hash to the screen
+	# print the hash to the screen
 
-# ask the user if all the info is correct
-	# if no...
-		# ask which field is incorrect
-		# update it 
-	# if yes...
-		# move on
+	# ask the user if all the info is correct
+		# if no...
+			# ask which field is incorrect
+			# update it 
+		# if yes...
+			# move on
 
-# print the hash again
+	# print the hash again
+
+end 
