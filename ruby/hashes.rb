9c3585f92711157b572b9d35ee1e10e73c55f8bf
Chris Johnson <johnsonc.88@gmail.com>
2016-07-19 14:06:36 -0700

    convert last response to boolean

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,5 +1,14 @@
 # Program to store client details
 
+# make a method to convert y/n to booleans
+def truth(a)
+	if a == "y"
+		return true
+	else
+		return false
+	end
+end
+
 # make a method to ask for and store a users...
 def Client_info
 	info = {}
@@ -20,7 +29,7 @@ def Client_info
 	# ...do they think lead poisioning is a myth
 	puts "You don't buy that whole 'lead is bad for you' malarkey do you?(y/n)"
 		# convert answer to boolean
-		info[:lead] = gets.chomp
+		info[:lead] = truth(gets.chomp)
 
 	# print the hash to the screen
 
