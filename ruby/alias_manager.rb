3135d17c144f97f825cf603bf508375dfd0bd46a
Chris Johnson <johnsonc.88@gmail.com>
2016-07-22 17:39:12 -0700

    make change_vowels method

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,6 +1,41 @@
+# helper function to change vowels
+def change_vowels(word)
+	vowels = 'aeiou'.split('')
+	letters = word.split('')
+	new_word = ''
+	# look each letter in turn
+	letters.each do |letter|
+		# if the letter is a vowel
+		if vowels.include? letter
+			# look at each vowel in turn
+			vowels.each do |vowel|
+				# if the letter is any of the vowels
+				if letter == vowel && vowel != 'u'
+					# change the letter to the next vowel
+					new_word << vowels[vowels.index(vowel) + 1]
+					# if the vowel is 'u' change it to 'a'
+				elsif letter == vowel && letter == 'u'
+					new_word << 'a'
+				end
+			end
+		else
+			new_word << letter
+		end		
+	end
+	return new_word
+end
+
+
+
 # take a name
-# break the first and last into array elements
-# swap their order
-# look for vowels
-# change vowels to the next vowel in 'aeiou'
-# put them back together
+def alias_maker(name)
+	# Isolate the first and last names
+	last_name = name.split(' ')[1]
+	first_name = name.split(' ')[0]
+	# swap their order
+	
+	# change vowels to the next vowel in 'aeiou'
+	# put them back together
+end
+
+puts change_vowels('appleu')
