308986fc3af39896617013596c04326fcb3ba37e
Chris Johnson <johnsonc.88@gmail.com>
2016-07-25 16:55:24 -0700

    make and test more methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -19,6 +19,38 @@ class Santa
 		puts "That was a good #{cookie}!" 
 	end
 
+	def celebrate_birthday
+		@age += 1
+	end
+
+	def get_mad_at(reindeer)
+		reindeer_ranking.delete(reindeer)
+		reindeer_ranking << reindeer
+	end
+
+	# setter method
+	def gender=(new_gender)
+		@gender = new_gender
+	end
+
+	# getter methods
+	def age
+		@age
+	end
+
+	def ethnicity
+		@ethnicity
+	end
+
+	def reindeer_ranking
+		@reindeer_ranking
+	end
+
+	def gender
+		@gender
+	end
+
+	# bonus method
 	def introduction
 		puts "Hi! My name is santa, I'm a #{@ethnicity} #{@gender} who is #{@age} years old and I'm chock full of holiday spirit!"
 	end
@@ -30,6 +62,17 @@ test = Santa.new('male', 'etheopian')
 test.speak
 test.eat_milk_and_cookies("oreo")
 test.introduction
+p test.age
+test.celebrate_birthday
+p test.age
+test.get_mad_at("Dasher")
+p test.reindeer_ranking
+p test.gender
+test.gender = "female"
+p test.gender
+p test.age
+p test.ethnicity
+
 
 # example input
 genders = ['male', 'female', 'bigender', 'prefer not to say']
