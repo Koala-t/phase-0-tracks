a8f04daefafecd113464ed030ff4c2fb61c4fa10
Chris Johnson <johnsonc.88@gmail.com>
2016-07-25 15:21:13 -0700

    make santa class

---
+++ ruby/santa.rb
@@ -0,0 +1,22 @@
+# make a santa class
+# Give it an initialize method and 2 other methods
+
+class Santa
+
+	def initialize
+		puts "Initializing Santa instance ..."
+	end
+
+	def speak
+		puts "Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie)
+		puts "That was a good #{cookie}!" 
+	end
+
+end
+
+test = Santa.new
+test.speak
+test.eat_milk_and_cookies("oreo")
