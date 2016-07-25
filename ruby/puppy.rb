b44de62a6cbdeef56c43a21dacbabd8fc2ef7ed4
Chris Johnson <johnsonc.88@gmail.com>
2016-07-25 09:38:49 -0700

    design puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+=begin
+
+SPECIES --------------------
+Canus Adorabilis
+
+CHARACTERISTICS ----------------	
+Height: tiny
+Color: varies
+Friendly: yes
+Energy: manic
+
+BEHAVIOR -------------------
+Wag-tail
+Bite-fingers
+Romp
+Sleep
+	
+=end
