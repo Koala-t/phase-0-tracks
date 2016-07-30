6e6b5078fcf939c743085302451cca151642cc58
Chris Johnson <johnsonc.88@gmail.com>
2016-07-30 08:29:21 -0700

    make a list

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,1 +1,44 @@
-#initial commit
+# Method to create a list
+def make_list(items)
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # initialize an empty hash
+  list = Hash.new
+  # split string into an array
+  array = items.split(' ')
+  	# iterate through the array
+  	array.each do |item|
+  		# add each element to the list as a key
+  		# set default quantity of 0
+  		list[item.to_sym] = 0
+  	end
+  # print the list to the console [can you use one of your other methods here?]
+  p list
+  # output: [what data type goes here, array or hash?]
+  list
+end
+
+
+# Method to add an item to a list
+# input: item name and optional quantity
+def add_item(item, quantity, list)
+	# steps: 
+		# make a new key with the input quantity
+	list[:item] = list[:item] + quantity
+	# output:
+		# updated list
+	p list
+end
+
+
+
+make_list("grapes milk cheese")
+# add_item("apples", 4, new_list)
+
+
+
+
+
+
+
+
