27aad0644d1920d0497b541321f93ab8bd04cb1b
Chris Johnson <johnsonc.88@gmail.com>
2016-07-31 16:08:39 -0700

    add pseudocode for tests

---
+++ ruby/game/hangman_spec.rb
@@ -0,0 +1,11 @@
+require_relative 'hangman'
+
+describe Game do
+	let(:game) {Game.new}
+	# the game should
+	# initalize with a guessed_letters and guessed_words
+	# take a target word and set number_of_guesses to 2 times the length of the input word
+	# allow the user to guess if the word has ceratain letters
+	
+
+end
