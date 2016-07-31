094bddf5523a20f156d7de829fb6ef72d784ea6a
Chris Johnson <johnsonc.88@gmail.com>
2016-07-31 16:22:08 -0700

    make tests

--- ruby/game/hangman_spec.rb
+++ ruby/game/hangman_spec.rb
@@ -4,8 +4,17 @@ describe Game do
 	let(:game) {Game.new}
 	# the game should
 	# initalize with a guessed_letters and guessed_words
+	it "initialize" do
+		expect(game.guessed_letters).to eq []
+	end
 	# take a target word and set number_of_guesses to 2 times the length of the input word
+	it "take input" do
+		expect(game.target('tree')).to eq 8
+	end
 	# allow the user to guess if the word has ceratain letters
-	
+	it "guess letters" do
+		game.target('tree') # need this line otherwise the target will be nil
+		expect(game.guess('t')).to eq true
+	end
 
 end
