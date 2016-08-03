e20f1fb6283b4ba10228d3ca7b35a11e6ea89b88
Chris Johnson <johnsonc.88@gmail.com>
2016-08-03 14:37:31 -0700

    make and test findLongestWord method

--- js/algos.js
+++ js/algos.js
@@ -1,14 +1,24 @@
 // method to return the longest word from an array of strings
 
 // Declare method and let it take an array as an argument
+function findLongestWord(words) {
 	//make a variable to hold the longest word and assign it to an empty string
+	var longestWord = '';
 	// iterate through the input array
+	for(var i = 0; i < words.length; i++) {
 		// if the current word is longer than the 'longest word'
+		if(words[i].length > longestWord.length) {
 			// reassign the longest word to the current word
+			longestWord = words[i];	
+		};
+	};
 	// return the longest word
-
+	return longestWord;
+};
 
 
 // driver code
 // declare an array and assign it to a variable
+var testArray = ["word", "longestWord", "longWord"]
 // invoke the longest-word-method and log the result
+console.log(findLongestWord(testArray))	
