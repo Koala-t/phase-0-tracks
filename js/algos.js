2bbf3f2805d40fb160acfc4b9acd9804cd6b530d
Chris Johnson <johnsonc.88@gmail.com>
2016-08-03 14:32:20 -0700

    pseudocode longest-word method

---
+++ js/algos.js
@@ -0,0 +1,14 @@
+// method to return the longest word from an array of strings
+
+// Declare method and let it take an array as an argument
+	//make a variable to hold the longest word and assign it to an empty string
+	// iterate through the input array
+		// if the current word is longer than the 'longest word'
+			// reassign the longest word to the current word
+	// return the longest word
+
+
+
+// driver code
+// declare an array and assign it to a variable
+// invoke the longest-word-method and log the result
