88e41dc21153a96c6707d249494bca11b6e7d99b
Chris Johnson <johnsonc.88@gmail.com>
2016-08-03 12:20:29 -0700

    make method to reverse a string

--- js/explore.js
+++ js/explore.js
@@ -1,5 +1,15 @@
 // method to reverse a string
 // declare method
-// iterate backwards through the string
-	// build a new string letter-by-letter
-// return the new string
+function reverse(string) {
+	newString = "";
+	// iterate backwards through the string
+	for(var i = string.length - 1; i >= 0; i--) {
+		// build a new string letter-by-letter
+		newString = newString + string[i];
+	}
+	// return the new string
+	return newString;
+}
+
+// driver code
+console.log(reverse('hello'));
