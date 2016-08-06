599937d25173cbed55ae76822e3520321032bfb2
Chris Johnson <johnsonc.88@gmail.com>
2016-08-05 18:18:41 -0700

    populate an object with horses

--- js/data_structures.js
+++ js/data_structures.js
@@ -7,3 +7,18 @@ colors.push('sparkly');
 // add a name to horses
 horses.push('Teddy');
 
+
+// make an object made of horses of different colors
+
+// make a new object
+// populate the object with horses
+// assign those horses colors
+
+// create object
+var stable = {};
+// iterate through the arrays
+for(var i = 0; i < horses.length; i++) {
+	// assign current array-values to the object as a key and value 
+	stable[horses[i]] = colors[i];
+};
+
