6fc9bf5498df301708b4cc283f3a071456b474cf
Chris Johnson <johnsonc.88@gmail.com>
2016-08-05 17:50:26 -0700

    add new elements to the arrays

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,3 +1,9 @@
 var colors = ['blue', 'red', 'green', 'yellow'];
 
 var horses = ['Edd', 'Princess', 'Dagger', 'Silver'];
+
+// add a color to colors
+colors.push('sparkly');
+// add a name to horses
+horses.push('Teddy');
+
