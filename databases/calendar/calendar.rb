7ecd252d095d775b9d00c717c0ad1fa6f62d788a
Chris Johnson <johnsonc.88@gmail.com>
2016-08-10 11:36:23 -0700

    convert loose code to methods and add driver-code

--- databases/calendar/calendar.rb
+++ databases/calendar/calendar.rb
@@ -27,74 +27,93 @@ add_events = <<-EVENTS_TABLE
 	)
 EVENTS_TABLE
 
-db.execute(add_events)
-db.execute(add_week)
 
+def generate_tables(days, db, add_events, add_week)
+	db.execute(add_events)
+	db.execute(add_week)
+	add_days(days, db)
+end
+
+# make a days array
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
 # make a function to fill the week with days
-days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 def add_days(days, db)
 	days.each do |d|
 		db.execute("INSERT INTO week (day) VALUES (?)", [d])
 	end
 end
 
-# add days to the week table if they're not already there 
-if db.execute("SELECT id FROM week") == []
-	add_days(days, db)
-end
-
 # prompt the user to add an event
-puts "Would you like to add an event to the calendar? (y/n)"
-if gets.chomp == 'y'
-	puts "What day will the event take place?"
-	date = days.index(gets.chomp) + 1
-	puts "What time will it take place?"
-	hour = gets.chomp
-	puts "Please enter a short description of the event."
-	appointment = gets.chomp
-	puts "Is this an important event?"
-	alert = gets.chomp
-	db.execute("INSERT INTO events (event, time, urgent, week_id) VALUES (?, ?, ?, ?)", [appointment, hour, alert, date])
+def new_event(db, days)
+	puts "Would you like to add an event to the calendar? (y/n)"
+	if gets.chomp == 'y'
+		puts "What day will the event take place?"
+		date = days.index(gets.chomp) + 1
+		puts "What time will it take place?"
+		hour = gets.chomp
+		puts "Please enter a short description of the event."
+		appointment = gets.chomp
+		puts "Is this an important event?"
+		alert = gets.chomp
+		db.execute("INSERT INTO events (event, time, urgent, week_id) VALUES (?, ?, ?, ?)", [appointment, hour, alert, date])
+	end
 end
 
 # allow the user to access all the events 
-puts "Would you like to view your calendar? (y/n)"
-if gets.chomp == 'y'
-	plan = db.execute("SELECT week.day, events.time, events.event FROM week, events WHERE events.week_id = week.id")
-	plan.each do |event|
-		puts event.join('-')
+def access_events(db)
+	puts "Would you like to view your calendar? (y/n)"
+	if gets.chomp == 'y'
+		plan = db.execute("SELECT week.day, events.time, events.event FROM week, events WHERE events.week_id = week.id")
+		plan.each do |event|
+			puts event.join('-')
+		end
 	end
 end
 
 # allow the user to delete events
-puts "Would you like to delete an event? (y/n)"
-if gets.chomp == 'y'
-	puts "What day is the event on?"
-	day = gets.chomp
-	index = days.index(day) + 1
-	puts "Which event would you like to delete from #{day}?"
-	deleted = gets.chomp
-	db.execute("DELETE FROM events WHERE events.event = ? AND events.week_id = ?", [deleted, index])
+def delete_event(db, days)
+	puts "Would you like to delete an event? (y/n)"
+	if gets.chomp == 'y'
+		puts "What day is the event on?"
+		day = gets.chomp
+		index = days.index(day) + 1
+		puts "Which event would you like to delete from #{day}?"
+		deleted = gets.chomp
+		db.execute("DELETE FROM events WHERE events.event = ? AND events.week_id = ?", [deleted, index])
+	end
 end
 
 # remind the user of the important events
-puts '----------------------'
-puts "Dont forget to attend:"
-important = db.execute("SELECT week.day, events.time, events.event FROM week, events WHERE events.week_id = week.id AND events.urgent = 'true'")
-important.each do |event|
+def reminder(db)
 	puts '----------------------'
-	puts event.join('-')
+	puts "Dont forget to attend:"
+	important = db.execute("SELECT week.day, events.time, events.event FROM week, events WHERE events.week_id = week.id AND events.urgent = 'true'")
+	important.each do |event|
+		puts '----------------------'
+		puts event.join('-')
+	end
+	puts '----------------------'
 end
-puts '----------------------'
 
 # make something to clear the week and day
-puts "Would you like to clear the calendar? (y/n)"
-if gets.chomp == 'y'
-	puts "Are you sure you would like to clear the calendar? (y/n)"
+def clear_calendar(db)
+	puts "Would you like to clear the calendar? (y/n)"
 	if gets.chomp == 'y'
-		db.execute("DROP TABLE week")
-		db.execute("DROP TABLE events")
+		puts "Are you sure you would like to clear the calendar? (y/n)"
+		if gets.chomp == 'y'
+			db.execute("DROP TABLE week")
+			db.execute("DROP TABLE events")
+		end
 	end
 end
+
+# driver code
+generate_tables(days, db, add_events, add_week)
+access_events(db)
+new_event(db, days)
+access_events(db)
+delete_event(db, days)
+clear_calendar(db)
+reminder(db)
 
