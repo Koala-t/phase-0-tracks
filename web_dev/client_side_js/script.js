491482716605919316d451e5f23d6c49f7bcf536
Chris Johnson <johnsonc.88@gmail.com>
2016-08-21 11:17:31 -0700

    make a div and add it to the body

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -3,3 +3,8 @@ var title = document.getElementsByTagName('title')[0];
 title.innerHTML = 'test page';
 
 
+// make a div
+var element = document.createElement('div');
+// add it to the body
+var body = document.getElementById('main');
+body.appendChild(element);
