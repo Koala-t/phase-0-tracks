472f8875b6f4024fa5ede42c5f6e440a1dd3abfa
Chris Johnson <johnsonc.88@gmail.com>
2016-08-21 11:23:50 -0700

    add style to the new div

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -8,3 +8,10 @@ var element = document.createElement('div');
 // add it to the body
 var body = document.getElementById('main');
 body.appendChild(element);
+
+
+// give the new div some style
+element.style.width = '500px';
+element.style.height = '300px';
+element.style.border = '10px solid purple';
+element.style.backgroundColor = 'blue';
